5579ed5c2d35b54e4e603cfacfe09e61705f317e
Matthieu Guffroy <mattgu74@gmail.com>
2010-09-23 01:36:50 +0200

    correction d'une erreur de thomas, maintenant c'est presque parfait ^^

--- collision/src/recouvreux/com/CollisionView.java
+++ collision/src/recouvreux/com/CollisionView.java
@@ -43,7 +43,7 @@ public class CollisionView extends View {
         	 mySpritePos.x = this.getWidth() - mySprite.getBounds().width();
          } else if (mySpritePos.x <= 0) {
         	 mySpritePos.x = 0;
-         } else if (mPitch > 0 && mySpritePos.x+50 >= 100 && mySpritePos.y+50 >= 100 && mySpritePos.y <= 150) {
+         } else if (mPitch < 0 && mySpritePos.x+50 >= 100 && mySpritePos.y+50 >= 100 && mySpritePos.y <= 150) {
         	 mySpritePos.x = 49;
 	     } else if (mySpritePos.x <= 150 && mySpritePos.y+50 >= 100 && mySpritePos.y <= 150) {
 	    	 mySpritePos.x = 151;
@@ -55,7 +55,7 @@ public class CollisionView extends View {
               mySpritePos.y = this.getHeight() - mySprite.getBounds().height(); 
          } else if (mySpritePos.y <= 0) { 
              mySpritePos.y = 0;
-         } else if (mHeading > 0 && mySpritePos.y+50 >= 100 && mySpritePos.x+50 >= 100 && mySpritePos.x <= 150) {
+         } else if (mHeading < 0 && mySpritePos.y+50 >= 100 && mySpritePos.x+50 >= 100 && mySpritePos.x <= 150) {
         	 mySpritePos.y = 49;
          } else if (mySpritePos.y <= 150 && mySpritePos.x+50 >= 100 && mySpritePos.x <= 150) {
         	 mySpritePos.y = 151;
