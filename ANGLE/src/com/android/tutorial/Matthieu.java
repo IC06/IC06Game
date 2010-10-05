fe6f37cfa838a0ff02afa20c030f60d46056934f
Matthieu Guffroy <mattgu74@gmail.com>
2010-10-05 14:37:29 +0200

    joli carré à éviter

--- ANGLE/src/com/android/tutorial/Matthieu.java
+++ ANGLE/src/com/android/tutorial/Matthieu.java
@@ -3,7 +3,6 @@ package com.android.tutorial;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -14,14 +13,12 @@ import android.widget.FrameLayout;
 
 import com.android.angle.AngleActivity;
 import com.android.angle.AngleCircleCollider;
-import com.android.angle.AngleFont;
 import com.android.angle.AngleObject;
 import com.android.angle.AnglePhysicObject;
 import com.android.angle.AnglePhysicsEngine;
 import com.android.angle.AngleSegmentCollider;
 import com.android.angle.AngleSprite;
 import com.android.angle.AngleSpriteLayout;
-import com.android.angle.AngleString;
 import com.android.angle.AngleUI;
 import com.android.angle.FPSCounter;
 
@@ -36,7 +33,6 @@ import com.android.angle.FPSCounter;
 public class Matthieu extends AngleActivity
 {
 	private MyDemo mDemo;
-	private AngleObject mBoard;
 	
    private final SensorEventListener mListener = new SensorEventListener() 
    {
@@ -88,12 +84,40 @@ public class Matthieu extends AngleActivity
 			mSprite.draw(gl);
 			//Draw colliders (beware calls GC)
 			//>Dibujado de los lolisionadores (cuidado, llama al GC)
-			//drawColliders(gl);
+			drawColliders(gl);
 		}
 		
 		
 	};
+	
+	private class Mur extends AnglePhysicObject
+	{
+		private AngleSprite mSprite;
 
+		public Mur(AngleSpriteLayout layout)
+		{
+			super(4, 0);
+			mSprite=new AngleSprite(layout);
+			addSegmentCollider(new AngleSegmentCollider(-40, -40, 40, -40));
+			addSegmentCollider(new AngleSegmentCollider(40, -40, 40, 40));
+			addSegmentCollider(new AngleSegmentCollider(40, 40, -40, 40));
+			addSegmentCollider(new AngleSegmentCollider(-40, 40, -40, -40));
+			mMass = 0;
+			mBounce = 0.8f; // Coefficient of restitution (1 return all the energy)  >Coeficiente de restituci�n (1 devuelve toda la energia)
+		}
+
+		@Override
+		public void draw(GL10 gl)
+		{
+			mSprite.mPosition.set(mPosition);
+			mSprite.draw(gl);
+			drawColliders(gl);
+		}
+		
+		
+	};
+	
+	
 	private class MyDemo extends AngleUI
 	{
 		AngleSpriteLayout mBallLayout;
@@ -107,15 +131,9 @@ public class Matthieu extends AngleActivity
 			mPhysics.mViscosity = 0f; // Air viscosity >Viscosidad del aire
 			addObject(mPhysics);
 			
-			mBoard=addObject(new AngleObject());
-			//The dashboard background
-			AngleSpriteLayout slDash = new AngleSpriteLayout(mActivity.mGLSurfaceView, 320, 64, R.drawable.tilemap, 0, 32, 320, 64);
-			AngleSprite mDash=(AngleSprite)mBoard.addObject(new AngleSprite (slDash));
-			mDash.mPosition.set(160, 480-slDash.roHeight/2);
-			mDash.mAlpha=0.5f;
-
-			//Font and text
-			mDemo.setTexte(1.0f);
+			Mur mMur = new Mur (mBallLayout);
+			mMur.mPosition.set(160, 240);
+			mPhysics.addObject(mMur); // Down wall
 			
 			
 			// Add 4 segment colliders to simulate walls
@@ -123,7 +141,7 @@ public class Matthieu extends AngleActivity
 			AnglePhysicObject mWall = new AnglePhysicObject(1, 0);
 			mWall.mPosition.set(160, 479);
 			mWall.addSegmentCollider(new AngleSegmentCollider(-160, 0, 160, 0));
-			mWall.mBounce = 0.5f;
+			mWall.mBounce = 1.5f;
 			mPhysics.addObject(mWall); // Down wall
 			
 			mWall = new AnglePhysicObject(1, 0);
@@ -155,13 +173,13 @@ public class Matthieu extends AngleActivity
 		public boolean onTouchEvent(MotionEvent event)
 		{
 			//Pour l'emulateur
-			mDemo.setGravity(-10,0);
+			mDemo.setGravity(0,10);
 			
 			if (event.getAction()==MotionEvent.ACTION_DOWN)
 			{
 				if ((event.getX()>30)&&(event.getY()>30)&&(event.getX()<320-30)&&(event.getY()<480-30))
 				{
-					mDemo.setTexte(event.getX());
+					//mDemo.setTexte(event.getX());
 					Ball mBall = new Ball (mBallLayout);
 					mBall.mPosition.set(event.getX(), event.getY());
 					// Ensure that there isn't any ball in this place
@@ -183,12 +201,6 @@ public class Matthieu extends AngleActivity
 		public void setGravity(float x, float y)
 		{
 			mPhysics.mGravity.set(x,y);
-		}
-		
-		public void setTexte(float x)
-		{
-			AngleFont fntCafe25 = new AngleFont(mActivity.mGLSurfaceView, 25, Typeface.createFromAsset(getAssets(),"cafe.ttf"), 222, 0, 0, 30, 200, 255, 255);
-			mBoard.addObject(new AngleString(fntCafe25,Float.toString(x),160,440,AngleString.aCenter));
 		}
 		
 	}
