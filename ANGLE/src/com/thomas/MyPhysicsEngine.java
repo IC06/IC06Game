0f4a570a61cf13158763fbe37942dc1933c5dc8e
Matthieu Guffroy <mattgu74@gmail.com>
2010-10-12 18:05:38 +0200

    gestion collision balle/plateforme meme couleur

--- ANGLE/src/com/thomas/MyPhysicsEngine.java
+++ ANGLE/src/com/thomas/MyPhysicsEngine.java
@@ -7,6 +7,7 @@ import com.android.angle.AnglePhysicObject;
 import com.android.angle.AnglePhysicsEngine;
 import com.android.angle.AngleSurfaceView;
 import com.android.angle.AngleVector;
+import com.thomas.Ball.Color;
 
 /**
  * @author thomas
@@ -60,8 +61,45 @@ public class MyPhysicsEngine extends AnglePhysicsEngine
 	@Override
 	protected void physics(float secondsElapsed)
 	{
-		super.physics(secondsElapsed);
+		for (int o = 0; o < mChildsCount; o++)
+		{
+			if (mChilds[o] instanceof Ball)
+			{
+				Ball mChildO = (Ball) mChilds[o];
+				// Gravity
+				//mChildO.mVelocity.mX += mChildO.mMass * mGravity.mX * secondsElapsed;
+				mChildO.mVelocity.mY += mChildO.mMass * mGravity.mY * secondsElapsed;
+				/*if ((mChildO.mVelocity.mX != 0) || (mChildO.mVelocity.mY != 0))
+				{
+					// Air viscosity
+					if (mViscosity > 0)
+					{
+						float surface = mChildO.getSurface();
+						if (surface > 0)
+						{
+							float decay = surface * mViscosity * secondsElapsed;
+							if (mChildO.mVelocity.mX > decay)
+								mChildO.mVelocity.mX -= decay;
+							else if (mChildO.mVelocity.mX < -decay)
+								mChildO.mVelocity.mX += decay;
+							else
+								mChildO.mVelocity.mX = 0;
+							if (mChildO.mVelocity.mY > decay)
+								mChildO.mVelocity.mY -= decay;
+							else if (mChildO.mVelocity.mY < -decay)
+								mChildO.mVelocity.mY += decay;
+							else
+								mChildO.mVelocity.mY = 0;
+						}
+					}
+				}*/
+				// Velocity
+				mChildO.mDelta.mX = mChildO.mVelocity.mX * secondsElapsed;
+				mChildO.mDelta.mY = mChildO.mVelocity.mY * secondsElapsed;
+			}
+		}
 	}
+
 
 	@Override
 	protected void kynetics(float secondsElapsed)
@@ -99,13 +137,16 @@ public class MyPhysicsEngine extends AnglePhysicsEngine
 							if (mChilds[c] instanceof Plateforme && mChildO.mVelocity.mY > 0) // l'objet est de type plateforme
 							{
 								Plateforme mChildC = (Plateforme) mChilds[c];
-								if (mChildO.collide(mChildC))
+								if(mChildC.mColor == mChildO.mColor || mChildC.mColor == Color.TOUTE)
 								{
-									mChildO.mPosition.mX -= mChildO.mDelta.mX;
-									mChildO.mVelocity.mY = -3 * mChildO.mRadius * 2; // la balle rebondit toujours de la même hauteur (simule un saut)
-									mChildC.mDelta.mX = mChildC.mVelocity.mX * secondsElapsed;
-									mChildC.mDelta.mY = mChildC.mVelocity.mY * secondsElapsed;
-									break;
+									if (mChildO.collide(mChildC))
+									{
+										mChildO.mPosition.mX -= mChildO.mDelta.mX;
+										mChildO.mVelocity.mY = -3 * mChildO.mRadius * 2; // la balle rebondit toujours de la même hauteur (simule un saut)
+										mChildC.mDelta.mX = mChildC.mVelocity.mX * secondsElapsed;
+										mChildC.mDelta.mY = mChildC.mVelocity.mY * secondsElapsed;
+										break;
+									}
 								}
 							}
 							else if (!(mChilds[c] instanceof Plateforme) && mChilds[c] instanceof AnglePhysicObject)
