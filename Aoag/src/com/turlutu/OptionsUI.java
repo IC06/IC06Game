9c29c6f7450ca2ad9dad49df4929725b00132406
Matthieu Guffroy <mattgu74@gmail.com>
2010-12-05 17:37:23 +0100

    start work

--- Aoag/src/com/turlutu/OptionsUI.java
+++ Aoag/src/com/turlutu/OptionsUI.java
@@ -1,9 +1,14 @@
 package com.turlutu;
 
+import android.app.Dialog;
 import android.graphics.Typeface;
 import android.os.Looper;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.angle.AngleActivity;
@@ -13,6 +18,8 @@ import com.android.angle.AngleSprite;
 import com.android.angle.AngleSpriteLayout;
 import com.android.angle.AngleString;
 import com.android.angle.AngleUI;
+import com.turlutu.ScoresUI.CancelListener;
+import com.turlutu.ScoresUI.OKListener;
 
 public class OptionsUI  extends AngleUI
 {
@@ -74,22 +81,64 @@ public class OptionsUI  extends AngleUI
 	
 	private void setSensibility()
 	{
-		new Thread() 
-		{
-			@Override 
-			public void run() 
-			{
-				Looper.prepare();
-				// TODO
-				Looper.loop();
-			}
-		}.start();
+
 	}
 	
 	@Override
 	public void onActivate()
 	{
 		super.onActivate();
+		new Thread() 
+		{
+			@Override 
+			public void run() 
+			{
+				Looper.prepare();
+				askParameter();
+				Looper.loop();
+			}
+		}.start();
+	}
+	
+	public void askParameter() {
+		Dialog dialog = new Dialog(mActivity);
+        dialog.setContentView(R.layout.name_activity);
+        dialog.setTitle("Entrer votre nom :");
+        Button buttonOK = (Button) dialog.findViewById(R.id.ok);        
+        buttonOK.setOnClickListener(new OKListener(dialog));
+        Button buttonCancel = (Button) dialog.findViewById(R.id.cancel);        
+        buttonCancel.setOnClickListener(new CancelListener(dialog));
+        dialog.show();
+	}
+	protected class OKListener implements OnClickListener {	 
+        private Dialog dialog;
+        public OKListener(Dialog dialog) {
+                this.dialog = dialog;
+        }
+
+        public void onClick(View v) {
+        		TextView input = (TextView) dialog.findViewById(R.id.entry);
+        		CharSequence name = input.getText();
+        		dialog.dismiss(); 
+        		DBScores db = new DBScores(mActivity);
+        		db.open();
+        		long i = db.insertScore( ((MainActivity) mActivity).mGame.mScore, ""+name);
+        		Log.i("ScoresUI", "ScoresUI on click on ok insert : " + i);
+        		db.close();
+        		((MainActivity) mActivity).mGame.mScore = 0;
+        }
+	}
+	
+	protected class CancelListener implements OnClickListener {	 
+        private Dialog dialog;
+        public CancelListener(Dialog dialog) {
+                this.dialog = dialog;
+        }
+
+        public void onClick(View v) {
+                dialog.dismiss();    
+        		((MainActivity) mActivity).mGame.mScore = 0;
+        }
 	}
 	
 	@Override
