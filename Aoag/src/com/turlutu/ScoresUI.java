f82f93b24e5747d80fda742a026db7cf69916cf9
Matthieu Guffroy <mattgu74@gmail.com>
2010-12-14 18:59:42 +0100

    Supression dans scoresUI des choses traités par gameoverui

--- Aoag/src/com/turlutu/ScoresUI.java
+++ Aoag/src/com/turlutu/ScoresUI.java
@@ -1,14 +1,8 @@
 package com.turlutu;
 
-import android.app.Dialog;
 import android.database.Cursor;
-import android.os.Looper;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.angle.AngleActivity;
@@ -65,14 +59,6 @@ public class ScoresUI   extends AngleUI
 		{
 			Log.i("ScoresUI", "Dialog show");
 			strNewScore.set("Last Score : " + ((MainActivity)mActivity).mGame.mScore);
-			DBScores db = new DBScores(mActivity);
-			db.open();
-			int worstscore = db.getWorstScore();
-			int nbScore = db.nbScore();
-			db.close();
-			if( ((MainActivity) mActivity).mGame.mScore > worstscore || nbScore < 8) {
-				askName();
-			}
 		}
 		Log.i("ScoresUI", "ScoresUI onActivate fin");
 	}
@@ -94,62 +80,6 @@ public class ScoresUI   extends AngleUI
         db.close();
         strScores.set(scores);
         strNames.set(names);
-	}
-	
-	public void askName() {
-		new Thread() 
-		{
-			@Override 
-			public void run() 
-			{
-				Looper.prepare();
-				Dialog dialog = new Dialog(mActivity);
-		        dialog.setContentView(R.layout.name_activity);
-		        dialog.setTitle("Enter your name :");
-		        TextView inputText = (TextView) dialog.findViewById(R.id.entry);
-		        inputText.setText(((MainActivity)mActivity).mOptions.mName);
-		        Button buttonOK = (Button) dialog.findViewById(R.id.ok);        
-		        buttonOK.setOnClickListener(new OKListener(dialog));
-		        Button buttonCancel = (Button) dialog.findViewById(R.id.cancel);        
-		        buttonCancel.setOnClickListener(new CancelListener(dialog));
-		        dialog.show();
-				Looper.loop();
-			}
-		}.start();
-	}
-	protected class OKListener implements OnClickListener {	 
-        private Dialog dialog;
-        public OKListener(Dialog dialog) {
-                this.dialog = dialog;
-        }
-
-        public void onClick(View v) {
-        		TextView input = (TextView) dialog.findViewById(R.id.entry);
-        		String name = ""+input.getText();
-        		if (name == "")
-        			name = "anonyme";
-        		((MainActivity)mActivity).mOptions.mName = name;
-        		dialog.dismiss(); 
-        		DBScores db = new DBScores(mActivity);
-        		db.open();
-        		long i = db.insertScore( ((MainActivity) mActivity).mGame.mScore, name);
-        		Log.i("ScoresUI", "ScoresUI on click on ok insert : " + i);
-        		db.close();
-        		((MainActivity) mActivity).mGame.mScore = 0;
-        		showScores();
-        }
-	}
-	
-	protected class CancelListener implements OnClickListener {	 
-        private Dialog dialog;
-        public CancelListener(Dialog dialog) {
-                this.dialog = dialog;
-        }
-
-        public void onClick(View v) {
-                dialog.dismiss();    
-        		((MainActivity) mActivity).mGame.mScore = 0;
-        }
 	}
 
 	public void DisplayScore(Cursor c)
